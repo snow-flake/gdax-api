492d8fa3e0f96ecb704f415476d0fc0f585840ee
Glenn <glenn+snow_flake@mercury-wireless.com>
2017-07-19 06:05:40 -0400

    Updated tests for account

--- clients/private_client.go
+++ clients/private_client.go
@@ -1,9 +1,72 @@
 package clients
 
 import (
+	"fmt"
 	"net/url"
 	"time"
 )
+
+type AccountReportStatus struct {
+	ID          string    `json:"id"`
+	Type        string    `json:"type"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	CompletedAt time.Time `json:"completed_at"`
+	ExpiresAt   time.Time `json:"expires_at"`
+	FileURL     string    `json:"file_url"`
+}
+
+/*
+	Get report status
+	HTTP REQUEST
+		GET /reports/:report_id
+
+	HTTP RESPONSE
+	Response (creating report)
+	{
+		"id": "0428b97b-bec1-429e-a94c-59232926778d",
+		"type": "fills",
+		"status": "creating",
+		"created_at": "2015-01-06T10:34:47.000Z",
+		"completed_at": undefined,
+		"expires_at": "2015-01-13T10:35:47.000Z",
+		"file_url": undefined,
+		"params": {
+			"start_date": "2014-11-01T00:00:00.000Z",
+			"end_date": "2014-11-30T23:59:59.000Z"
+		}
+	}
+	Response (finished report)
+	{
+		"id": "0428b97b-bec1-429e-a94c-59232926778d",
+		"type": "fills",
+		"status": "ready",
+		"created_at": "2015-01-06T10:34:47.000Z",
+		"completed_at": "2015-01-06T10:35:47.000Z",
+		"expires_at": "2015-01-13T10:35:47.000Z",
+		"file_url": "https://example.com/0428b97b.../fills.pdf",
+		"params": {
+			"start_date": "2014-11-01T00:00:00.000Z",
+			"end_date": "2014-11-30T23:59:59.000Z"
+		}
+	}
+
+	Once a report request has been accepted for processing, the status is available by polling the report resource endpoint.
+	The final report will be uploaded and available at file_url once the status indicates ready
+
+	STATUS
+	| Status | Description |
+	| pending | The report request has been accepted and is awaiting processing |
+	| creating | The report is being created |
+	| ready | The report is ready for download from file_url |
+*/
+func GetAccountReportStatus(client *Client, report_id string) (*AccountReportStatus, error) {
+	pathname := fmt.Sprintf("/reports/%s", report_id)
+	params := url.Values{}
+	output := &AccountReportStatus{}
+	_, err := client.Get(pathname, params, &output)
+	return output, err
+}
 
 type AccountTrailingVolume struct {
 	ProductID      string    `json:"product_id"`
--- clients/private_client_test.go
+++ clients/private_client_test.go
@@ -11,6 +11,72 @@ import (
 //
 //
 
+func Test_GetAccountReportStatus(t *testing.T) {
+	// Setup the mocks
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	// Mock the time request
+	httpmock.RegisterResponder(
+		"GET",
+		"https://mock-api.gdax.com/reports/0428b97b-bec1-429e-a94c-59232926778d",
+		httpmock.NewStringResponder(
+			200,
+			`
+				{
+					"id": "0428b97b-bec1-429e-a94c-59232926778d",
+					"type": "fills",
+					"status": "creating",
+					"created_at": "2015-01-06T10:34:47.000Z",
+					"expires_at": "2015-01-13T10:35:47.000Z",
+					"params": {
+						"start_date": "2014-11-01T00:00:00.000Z",
+						"end_date": "2014-11-30T23:59:59.000Z"
+					}
+				}
+			`,
+		),
+	)
+	client := NewMockClient()
+	output, err := GetAccountReportStatus(client, "0428b97b-bec1-429e-a94c-59232926778d")
+	expected := AccountReportStatus{
+		ID:          "0428b97b-bec1-429e-a94c-59232926778d",
+		Type:        "fills",
+		Status:      "creating",
+		CreatedAt:   time.Date(2015, 01, 06, 10, 34, 47, 0, time.UTC),
+		CompletedAt: time.Date(0001, 01, 01, 00, 00, 00, 0, time.UTC),
+		ExpiresAt:   time.Date(2015, 01, 13, 10, 35, 47, 0, time.UTC),
+		FileURL:     "",
+	}
+	if err != nil {
+		t.Fatalf("Error should be nil, %v", err)
+	}
+	if output == nil {
+		t.Fatalf("Expected output to not be nil, actual = %v", output)
+	}
+	if !reflect.DeepEqual(output.ID, expected.ID) {
+		t.Fatalf("Expected output.ID %v to match expected.ID %v", output.ID, expected.ID)
+	}
+	if !reflect.DeepEqual(output.Type, expected.Type) {
+		t.Fatalf("Expected output.Type %v to match expected.Type %v", output.Type, expected.Type)
+	}
+	if !reflect.DeepEqual(output.Status, expected.Status) {
+		t.Fatalf("Expected output.Status %v to match expected.Status %v", output.Status, expected.Status)
+	}
+	if !reflect.DeepEqual(output.CreatedAt.UTC().Unix(), expected.CreatedAt.UTC().Unix()) {
+		t.Fatalf("Expected output.CreatedAt %v to match expected.CreatedAt %v", output.CreatedAt, expected.CreatedAt)
+	}
+	if !reflect.DeepEqual(output.CompletedAt.UTC().Unix(), expected.CompletedAt.UTC().Unix()) {
+		t.Fatalf("Expected output.CompletedAt %v to match expected.CompletedAt %v", output.CompletedAt, expected.CompletedAt)
+	}
+	if !reflect.DeepEqual(output.FileURL, expected.FileURL) {
+		t.Fatalf("Expected output.FileURL %v to match expected.FileURL %v", output.FileURL, expected.FileURL)
+	}
+}
+
+//
+//
+//
+
 func Test_mock_GetAccountTrailingVolume(t *testing.T) {
 	// Setup the mocks
 	httpmock.Activate()
